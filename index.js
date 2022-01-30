5eff23375ef1c4e9dccc460d6caaa931e6195883
Amarnath0510 <amarmuthu007@gmail.com>
2022-01-30 09:55:55 +0530

    organized

--- index.js
+++ index.js
@@ -3,101 +3,18 @@ import { MongoClient } from "mongodb";
 import cors from 'cors';
 import dotenv from "dotenv";
 import { getGoldsById, createGolds, getGolds, deleteGoldsById, updateGoldById } from "./helper.js";
+import {goldsRouter} from "./routes/golds.js"
 dotenv.config();
 console.log(process.env);
 
 
 const app = express();
-const PORT = 10000;
+const PORT =process.env.PORT;
 app.use(cors());
 app.use(express.json());
-// const golds = [
-//   {
-//     id: "100",
-//     name: "Geometrical Bracelet",
-//     picture:
-//       "https://static-01.daraz.com.bd/p/28a4c0ddc62f26de1522f84d9fb270c4.jpg",
 
-//     weight: "16 grams",
-//     availability: "In stock",
-//     price: " M.R.P ₹82560",
-//   },
-//   {
-//     id: "101",
-//     name: "Mangalam Gold bangles",
-//     picture:
-//       "https://i.pinimg.com/originals/2a/1e/c3/2a1ec37597a1b2092e31ffb85893302e.jpg",
 
-//     weight: "24 grams",
-//     availability: "In stock",
-//     price: "M.R.P ₹112360",
-//   },
 
-//   {
-//     id: "102",
-//     name: "Sparkling Gold Ring",
-//     picture: "https://ae01.alicdn.com/kf/HTB1k4CAXU_rK1Rjy0Fcq6zEvVXar.jpg",
-
-//     weight: "4g grams",
-//     availability: "In stock",
-//     price: "M.R.P ₹19680",
-//   },
-//   {
-//     id: "103",
-//     name: "Bewitch Studded necklace",
-//     picture:
-//       "https://www.dailyexcelsior.com/wp-content/uploads/2020/09/necklace.jpg",
-
-//     weight: "24 grams",
-//     availability: "Make to order",
-//     price: "M.R.P ₹103237",
-//   },
-//   {
-//     id: "104",
-//     name: "Gilded Gold Earring",
-//     picture:
-//       "https://i.pinimg.com/originals/71/86/04/718604e669d5a650f361774bcde04de9.jpg",
-
-//     weight: "11 grams",
-//     availability: "In stock",
-//     price: "M.R.P ₹45968",
-//   },
-
-//   {
-//     id: "105",
-//     name: "22Kt  Cutwork Mens Chain",
-//     picture:
-//       "https://www.anuradhaartjewellery.com/assets/images/product/100757/original/CH-176_NET_RS-3800_Q-2_31_white.jpeg",
-
-//     weight: "22 grams",
-//     availability: "In stock",
-//     price: "M.R.P ₹112000",
-//   },
-
-//   {
-//     id: "106",
-//     name: "Era uncut necklace set",
-//     picture:
-//       "https://i.pinimg.com/736x/bd/00/d3/bd00d3afac2857b336498ca6f2a1baff.jpg",
-
-//     weight: "32 grams",
-//     availability: "Make to order",
-//     price: "M.R.P ₹165023",
-//   },
-
-//   {
-//     id: "107",
-//     name: "Ethnix Gold Bangle Set",
-//     picture:
-//       "https://c0.wallpaperflare.com/preview/56/821/69/gold-desktop-celebration-jewelry.jpg",
-
-//     weight: "14.5 grams",
-//     availability: "In stock",
-//     price: "M.R.P ₹110000",
-//   },
-// ];
-
-// const MONGO_URL="mongodb://localhost";
 const MONGO_URL=process.env.MONGO_URL; 
 async function createConnection(){
   const client= new MongoClient(MONGO_URL);
@@ -113,9 +30,6 @@ app.get("/", (request, response) => {
 });
 
 app.use("/golds",goldsRouter)
-
-
-
 app.listen(PORT, () => console.log("App is started in", PORT));
 
 
--- routes/golds.js
+++ routes/golds.js
@@ -1,6 +1,17 @@
 import express from "express";
 const router=express.Router();
-app.get("/golds/:id",async (request, response) => {
+import
+{
+  getGoldsById,
+   createGolds,
+    getGolds, 
+    deleteGoldsById,
+     updateGoldById
+}from "../helper.js";
+
+router
+.route("/:id")
+.get(async (request, response) => {
     console.log(request.params);
     const { id } = request.params;
     const gold= await getGoldsById(id);
@@ -8,35 +19,17 @@ app.get("/golds/:id",async (request, response) => {
     gold
       ? response.send(gold)
       : response.status(404).send({ message: "No matching golds found" });
-  });
-  
-  app.post("/golds",async(request,response)=>{
-    const data=request.body;
-    const result=await createGolds(data);
-    response.send(result);
   })
-  
-  app.get("/golds", async(request, response) => {
-    console.log(request.query);
-    const filter=request.query;
-    console.log(filter);
-  
-    
-  const filterGolds= await getGolds(filter);
-    response.send(filterGolds);
-  });
-  
-  app.delete("/golds/:id",async(request,response)=>{
+  .delete(async(request,response)=>{
     console.log(request.params);
     const{id}=request.params;
   const result= await deleteGoldsById(id);
   result.deletedCount > 0
   ? response.send(result)
   : response.status(404).send({message:"No matching golds found"})
-  });
-  
-  
-  app.put("/golds/:id",async(request,response)=>{
+  })
+
+  .put(async(request,response)=>{
     console.log(request.params);
     const{id}=request.params;
     const data=request.body;
@@ -44,3 +37,24 @@ app.get("/golds/:id",async (request, response) => {
   const gold=await getGoldsById(id);
   response.send(gold);
   })
+
+  router
+  .route("/")
+  .get( async(request, response) => {
+    console.log(request.query);
+    const filter=request.query;
+    console.log(filter);
+  
+    
+  const filterGolds= await getGolds(filter);
+    response.send(filterGolds);
+  })
+  
+  .post(async(request,response)=>{
+    const data=request.body;
+    const result=await createGolds(data);
+    response.send(result);
+  })
+  
+
+  export const goldsRouter=router;
